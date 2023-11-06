83c8af39e87b88d54bfce8535728f16f706f65d9
Helvizar <helvizaranderson@gmail.com>
2023-11-06 22:08:40 +0700

    feat: implemented HTML template filling

--- index.js
+++ index.js
@@ -33,6 +33,27 @@ const url = require("url");
 
 // read file, only read file once and only at the beginning when the program started, so it using synchronous version.
 // Top-level code.
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%PRODUCTID%}/g, product.id);
+  
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  }
+
+  return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 // fs.readFile('./dev-data/data.json'); // another way to specify path name
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 // convert from json to javascript
@@ -44,7 +65,12 @@ const server = http.createServer((req, res) => {
 
   // create routing
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is OVERVIEW");
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cardsHtml = dataObject.map(element => replaceTemplate(tempCard, element)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+    res.end(output);
   } else if (pathName === "/product") {
     res.end("This is PRODUCT");
   } else if (pathName === "/api") {
